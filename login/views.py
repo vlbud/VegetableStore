9e6e99e76ecc9d6b9482ddd767c6df5d7ecacb74
vlad <buyou.self@mail.ru>
2023-06-22 22:24:59 +0300

    Работа с формой регистрации

--- login/views.py
+++ login/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
 from django.views import View
 from django.http import JsonResponse
 
@@ -10,3 +12,19 @@ class LoginView(View):
 
     def post(self, request):
         return JsonResponse(request.POST, json_dumps_params={"indent": 4})
+
+    def post(self, request):
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('store:shop')
+        return redirect('login:login')
+class LogoutView(View):
+   def get(self, request):
+       if request.user.is_authenticated:
+           logout(request)
+       return redirect('store:shop')
