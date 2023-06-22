f8f1f8a12a0eb1cac34039696859d43ee335b267
vlad <buyou.self@mail.ru>
2023-06-22 22:59:41 +0300

    Работа с отладкой начало

--- login/urls.py
+++ login/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import LoginView
-from .views import LogoutView
+from .views import LogoutView, CreateAccountView,LoginView
+
 
 app_name = 'login'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', LoginView.as_view()),
     path('', LoginView.as_view(), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
+    path('create/', CreateAccountView.as_view(), name="create"),
 ]
